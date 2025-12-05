b5d5e9401c45d4f201494b2a0df49c6a3b8a6f7b
Anurag-Bachheti <anuragbachheti1999@gmail.com>
2025-12-05 10:18:26 +0530

    Added new changes to test branch

--- src/Layout/Footer.jsx
+++ src/Layout/Footer.jsx
@@ -4,6 +4,7 @@ const Footer = () => {
     return (
         <>
             <h1>Footer</h1>
+            <p>This is my footer, it will contain all the extra comments and company details.</p>
         </>
     )
 }
